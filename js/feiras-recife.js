e88078e97b417608f444312f3be2693e3972effb
Marina Menezes <marinamenezes@MacBook-Air-de-Marina.local>
2024-01-06 21:19:27 -0300

    adiciona recife e links

---
+++ js/feiras-recife.js
@@ -0,0 +1,105 @@
+const feiras = [
+    {
+        "Endereço": "Praça Dom Vital",
+        "Bairro": "São José",
+        "Dia": "Quinta a sábado",
+        "Horário": "06:00 às 18:00"
+    },
+    {
+        "Endereço": "Rua Uzeda Luna",
+        "Bairro": "Água Fria",
+        "Dia": "Todos os dias",
+        "Horário": "06:00 às 18:00"
+    },
+    {
+        "Endereço": "Praça da Convenção",
+        "Bairro": "Boa Viagem",
+        "Dia": "Todos os dias",
+        "Horário": "06:00 às 18:00"
+    },
+    {
+        "Endereço": "Rua Ramis Galvão",
+        "Bairro": "Arruda",
+        "Dia": "Quinta a sábado",
+        "Horário": "06:00 às 18:00"
+    },
+    {
+        "Endereço": "Largo da Encruzilhada",
+        "Bairro": "Encruzilhada",
+        "Dia": "Segunda e terça-feira",
+        "Horário": "06:00 às 18:00"
+    },
+    {
+        "Endereço": "Rua Alto do Deodato",
+        "Bairro": "Água Fria",
+        "Dia": "Domingo",
+        "Horário": "06:00 às 12:00"
+    },
+
+    {
+        "Endereço": "Largo de Casa Amarela",
+        "Bairro": "Casa Amarela",
+        "Dia": "Todos os dias",
+        "Horário": "06:00 às 18:00"
+    },
+    {
+        "Endereço": "Av. Nova Descoberta",
+        "Bairro": "Nova Descoberta",
+        "Dia": "Todos os dias",
+        "Horário": "06:00 às 18:00",
+    },
+    {
+        "Endereço": "Rua Inácio Monteiro",
+        "Bairro": "Cordeiro",
+        "Dia": "Quarta e quinta-feira",
+        "Horário": "06:00 às 18:00"
+    },
+    {
+        "Endereço": "Pátio do Mercado do Cordeiro",
+        "Bairro": "Cordeiro",
+        "Dia": "Quinta a sábado",
+        "Horário": "06:00 às 20:00"
+    },
+    {
+        "Endereço": "Rua Azevedo Coutinho",
+        "Bairro": "Várzea",
+        "Dia": "Quinta-feira a sábado",
+        "Horário": "06:00 às 20:00"
+    },
+    {
+        "Endereço": "Rua Gov. Lupo Garro",
+        "Bairro": "Engenho Do Meio",
+        "Dia": "Sábados e domingos",
+        "Horário": "06:00 às 17:00"
+    },
+    {
+        "Endereço": "Rua Barros Sobrinho",
+        "Bairro": "Areias",
+        "Dia": "Sexta-feira e sábado",
+        "Horário": "06:00 às 18:00"
+    },
+    {
+        "Endereço": "Av. Manoel Gonçalves da Luz",
+        "Bairro": "Mustardinha",
+        "Dia": "Todos os dias",
+        "Horário": "06:00 às 18:00"
+    },
+    {
+        "Endereço": "Estrada dos Remédios, s/n",
+        "Bairro": "Afogados",
+        "Dia": "Todos os dias",
+        "Horário": "06:00 às 20:00"
+    },
+    {
+        "Endereço": "Rua Alberto Lundgren",
+        "Bairro": "Jordão",
+        "Dia": "Todos os dias",
+        "Horário": "06:00 às 18:00"
+    },
+    {
+        "Endereço": "Av. Pernambuco",
+        "Bairro": "Cohab",
+        "Dia": "Todos os dias",
+        "Horário": "06:00 às 18:00"
+    }
+]
